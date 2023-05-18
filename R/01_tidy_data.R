22de5bef2d2f5aa17d304d955c4d51f836910e87
egouldo <elliot.gould@unimelb.edu.au>
2023-05-18 10:49:03 -0400

    Convert Site, Habitat, Season to factor in bat_data

--- R/01_tidy_data.R
+++ R/01_tidy_data.R
@@ -1,2 +1,8 @@
 library(tidyverse)
 bat_data <- read_csv("data/bat_dat.csv")
+
+bat_data <- 
+  mutate(bat_data, 
+         Site = as.factor(Site),
+         Habitat = as.factor(Habitat),
+         Season = as.factor(Season))
